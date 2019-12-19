863086cfe12e24af3cc87ea4db5fa1348bf39b06
1Lighty <david.merkov@gmail.com>
2019-12-19 14:50:51 +0100

    Updates

---
+++ precache-manifest.b26963912f1c04113db65f683bdc9030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ae88878581897254801f547ff0ea11",
+    "url": "/BetterDiscordStuff/index.html"
+  },
+  {
+    "revision": "c95e29c34475f767abde",
+    "url": "/BetterDiscordStuff/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b75fd84ab5cc1ab31927",
+    "url": "/BetterDiscordStuff/static/js/2.c181463c.chunk.js"
+  },
+  {
+    "revision": "8ec8b65940d4527c3e1ef85a62fa0e11",
+    "url": "/BetterDiscordStuff/static/js/2.c181463c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c95e29c34475f767abde",
+    "url": "/BetterDiscordStuff/static/js/main.78cc5b96.chunk.js"
+  },
+  {
+    "revision": "761c453297bd818950be",
+    "url": "/BetterDiscordStuff/static/js/runtime-main.67e26751.js"
+  }
+]);
