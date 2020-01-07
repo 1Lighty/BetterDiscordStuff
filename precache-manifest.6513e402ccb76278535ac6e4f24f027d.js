bd4d69cf0461f124192d0e336d5a5506a1f3fa5a
1Lighty <david.merkov@gmail.com>
2020-01-07 12:15:02 +0000

    Updates

---
+++ precache-manifest.6513e402ccb76278535ac6e4f24f027d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09846ba4eb125d4d7f5e029b992d4116",
+    "url": "/BetterDiscordStuff/index.html"
+  },
+  {
+    "revision": "e735089f47757c43edf5",
+    "url": "/BetterDiscordStuff/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "9bcfbb86982852e3246c",
+    "url": "/BetterDiscordStuff/static/js/2.52f09d7a.chunk.js"
+  },
+  {
+    "revision": "8ec8b65940d4527c3e1ef85a62fa0e11",
+    "url": "/BetterDiscordStuff/static/js/2.52f09d7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e735089f47757c43edf5",
+    "url": "/BetterDiscordStuff/static/js/main.af60c2b8.chunk.js"
+  },
+  {
+    "revision": "761c453297bd818950be",
+    "url": "/BetterDiscordStuff/static/js/runtime-main.67e26751.js"
+  }
+]);
